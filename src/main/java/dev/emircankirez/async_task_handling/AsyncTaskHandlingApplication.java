b157c0aca1623f5b42ce30423638183e17ea07e2
Emircan Kirez <emircankirezz@gmail.com>
2025-07-03 19:12:12 +0300

    init project

---
+++ src/main/java/dev/emircankirez/async_task_handling/AsyncTaskHandlingApplication.java
@@ -0,0 +1,13 @@
+package dev.emircankirez.async_task_handling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncTaskHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncTaskHandlingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/emircankirez/async_task_handling/AsyncTaskHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.emircankirez.async_task_handling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncTaskHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
